b58d748ecc8f6cd2d8d7afbc22a31d3e6f461c98
mmato72 <119341627+mmato72@users.noreply.github.com>
2026-02-12 10:32:38 +0100

    Running ASCII Shell Script

---
+++ .github/workflows/ascii-script.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install cowsay -y
+cowsay -f dragon "Run from the dragon always ... safe into the arms of Jesus" >> dragon.txt
+grep -i "dragon" dragon.txt
+cat dragon.txt
+ls -ltra
